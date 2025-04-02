3141b197f909e5d134e0ab346b1f90111ab4e59f
d4ff33 <193672396+d4ff33@users.noreply.github.com>
2025-04-01 21:31:15 -0400

    Create dev notes for clicker game

--- scripts/script.js
+++ scripts/script.js
@@ -52,6 +52,21 @@ const concatStrings = () => {
     string1 + string2);
 };
 
+//Collapsinator, i stole this from w3 :3
+const collapsibles = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < collapsibles.length; i++) {
+  collapsibles[i].addEventListener("click", function () {
+    this.classList.toggle("collapsed");
+    const content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 //Click da alien game
 const clicker = {
   sfx: {
@@ -66,15 +81,25 @@ const clicker = {
   ui: {
     seconds: document.getElementById("clicker-seconds-remaining"),
     gameover: document.getElementById("clicker-gameover-screen"),
-    alien: document.getElementById("clicker-alien"),
+    alien: {
+      htmlElement: document.getElementById("clicker-alien"),
+      randomize: function () {
+        const posXRandomize = Math.random() * 500;
+        const posYRandomize = Math.random() * 500;
+        const widthRandomize = Math.random() * 100 + 50;
+        this.htmlElement.style.width = widthRandomize + "px";
+        this.htmlElement.style.top = posYRandomize + "px";
+        this.htmlElement.style.left = posXRandomize + "px";
+      },
+    },
   },
   game: {
     replay: function () {
       clicker.state.isActive = false;
       clicker.state.isCounting = false;
-      clicker.ui.alien.style.top = "45%";
-      clicker.ui.alien.style.left = "45%";
-      clicker.ui.alien.style.width = "50px";
+      clicker.ui.alien.htmlElement.style.top = "45%";
+      clicker.ui.alien.htmlElement.style.left = "45%";
+      clicker.ui.alien.htmlElement.style.width = "50px";
       clicker.counter.timer = 15;
       clicker.counter.clicks = 0;
       clicker.ui.seconds.value = clicker.counter.timer.toString();
@@ -99,28 +124,22 @@ const clicker = {
       }, 1000);
     },
     play: function () {
-      const posXRandomize = Math.random() * 500;
-      const posYRandomize = Math.random() * 500;
-      const widthRandomize = Math.random() * 100 + 50;
-
-      clicker.ui.alien.animate(
+      clicker.ui.alien.htmlElement.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+        }
+      });
+      clicker.ui.alien.htmlElement.animate(
         [{ transform: "scale(2)" }, { transform: "scale(1)" }],
         {
           duration: 100,
           easing: "linear",
         }
       );
-
-      const propertyRandomize = () => {
-        clicker.ui.alien.style.width = widthRandomize + "px";
-        clicker.ui.alien.style.top = posYRandomize + "px";
-        clicker.ui.alien.style.left = posXRandomize + "px";
-      };
-
       if (!clicker.state.isActive) {
         clicker.sfx.giggle.load();
         clicker.sfx.giggle.play();
-        propertyRandomize();
+        clicker.ui.alien.randomize();
         clicker.state.isActive = true;
         clicker.counter.clicks++;
         document.getElementById("clicker-click-count").value =
@@ -131,7 +150,7 @@ const clicker = {
       } else if (clicker.state.isActive && clicker.counter.timer >= 0) {
         clicker.sfx.giggle.load();
         clicker.sfx.giggle.play();
-        propertyRandomize();
+        clicker.ui.alien.randomize();
         clicker.counter.clicks++;
         document.getElementById("clicker-click-count").value =
           clicker.counter.clicks;
