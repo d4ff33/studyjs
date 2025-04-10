0449eae37f65f6764127237fc5eef6b6a8acfc43
d4ff33 <193672396+d4ff33@users.noreply.github.com>
2025-04-10 14:49:05 -0400

    Add section 3

--- scripts/eloquentjs.js
+++ scripts/eloquentjs.js
@@ -35,4 +35,17 @@ const exercises = {
       console.log(string);
     },
   },
+  section3: {
+    min: function (num1, num2) {
+      if (num1 >= num2) {
+        return num2;
+      } else if (num2 >= num1) {
+        return num1;
+      }
+    },
+    isEven: function (num) {
+      let even = 0;
+      let odd = 1;
+    },
+  },
 };
